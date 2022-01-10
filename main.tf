0ade36eee3a3bf22d4b01a222ed2a22bb4a09b3f
saikiranreddy1808 <saikiranreddyrmdv@gmail.com>
2022-01-10 12:04:39 +0530

    tf added

--- main.tf
+++ main.tf
@@ -11,3 +11,20 @@ provider "aws" {
   # Configuration options
   region="ap-south-1"
 }
+
+resource "aws_instance" "tf-2" {
+  ami           = "ami-052cef05d01020f1d"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "mytfinstance"
+  }
+}
+
+output "instance-ip"{
+  value = aws_instance.tf-2.public_ip
+}
+
+output "private-ip" {
+  value = aws_instance.tf-2.private_ip
+}
